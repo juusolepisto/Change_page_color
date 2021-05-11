a08f92f5d08915129bd2ec38bdc2868cb3f9c782
juusolepisto <juuso.lepisto@edu.turkuamk.fi>
2021-05-11 20:41:31 +0300

    init

--- change_color.js
+++ change_color.js
@@ -1,13 +1,12 @@
+var input = document.querySelectorAll("input");
 
-function changeColor() {
-    let red = document.getElementById('red').value;
-    let blue = document.getElementById('blue').value;
-    let green = document.getElementById('green').value;
-    let color = 'rgb(' + red + "," + green + "," + blue + ')';
-    document.p1.style.backgroundColor = color;
-    document.getElementById('colorOutput').innerHTML = ': ' + color;
+for(var i = 0; i < input.length; i++){
+    input[i].addEventListener("input", function(){
+        var red = document.getElementById("red").value,
+            green = document.getElementById("green").value,
+            blue = document.getElementById("blue").value;
+
+        var p1 = document.getElementById("p1");
+        display.style.background = "rgb(" + red + "," + green + "," + blue + ")";
+    });
 }
-
-document.getElementById('red').addEventListener('input', changeColor);
-document.getElementById('blue').addEventListener('input', changeColor);
-document.getElementById('green').addEventListener('input', changeColor);
